298a3ce0c8036ac757265417dd803d04cba6eddd
Dasadno <Yakaleks96@gmail.com>
2025-03-31 15:02:37 +0500

    поплачь

--- may/Main.cpp
+++ may/Main.cpp
@@ -12,8 +12,8 @@ int main ()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     Player player1("Харчок");
-    Player player2("Чушка");
-
+    Player player2("Сверчок");
+    Player player3("Шашлычок");
 
     Bush* bush = new Bush("Черничка", 1.5);
     bush->setBerryCount(50); 
@@ -23,34 +23,48 @@ int main ()
 
 
 
-    Berry* berry = new Berry("Клубничка", 0.5);
-    CommonFruit* apple = new CommonFruit("Яблочко", 0.2); 
-    Cone* pineCone = new Cone("Шишечка еловая", 0.1);
+    Berry* berry = new Berry("Клубнииичка:()", 0.1);
+    Berry* berrry = new Berry("Малииинка:^", 0.1);
+    Berry* berrrry = new Berry("Ежевичка:)))", 0.2);
+    CommonFruit* apple = new CommonFruit("Яблочкоооу;(", 0.3); 
+    CommonFruit* mandarinka = new CommonFruit("Мандааааринка^-^", 0.4);
+    Cone* pineCone = new Cone("Шишечка еловая:3", 0.2);
+    Cone* mayCone = new Cone("Шишечка пихтовая$", 0.1);
 
 
     player1.getBackpack().addFruit(berry);
     player1.getBackpack().addFruit(apple);
-
-
+    player1.getBackpack().addFruit(mandarinka);
 
     player2.getBackpack().addFruit(pineCone);
+    player2.getBackpack().addFruit(berrry);
 
 
-    std::cout << player1.getName() << " рюкзачок: " << std::endl;
+    player3.getBackpack().addFruit(berrrry);
+    player3.getBackpack().addFruit(mayCone);
+
+    std::cout << player1.getName() << " рюкзакчокчок: " << std::endl;
     player1.getBackpack().printContents();
 
-    std::cout << std::endl << player2.getName() << " рюкзачок: " << std::endl;
+    std::cout << std::endl << player2.getName() << " рюкзакчокчок: " << std::endl;
     player2.getBackpack().printContents();
+    std::cout << std::endl;
+    std::cout << player3.getName() << " рюкзакчокчок: " << std::endl;
+    player3.getBackpack().printContents();
     std::cout << std::endl;
 
     delete bush;
     delete tree;
 
     delete berry;
+    delete berrry;
+    delete berrrry;
+    delete mandarinka;
     delete apple;
     delete pineCone;
+    delete mayCone;
 
-    std::cout << "Мяу мяу, мы сходили в лес:)" << std::endl;
+    std::cout << "Мой дурук,мы с табой схадить в лес и сабрат многа палезных вкуснашек ииииуууу" << std::endl;
 
     return 0;
 }
